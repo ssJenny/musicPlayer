82b9b72f084174f6e00b1bbe926b31f2923864c2
cissy <sissi_sx@163.com>
2017-07-11 11:50:24 +0800

    获取歌手列表

--- src/components/singer/singer.vue
+++ src/components/singer/singer.vue
@@ -1,11 +1,106 @@
 <template>
-  <div>
-    歌手
+  <div class="singer" ref="singer">
+    <list-view :data="singers" ref="list"></list-view>
+    <router-view></router-view>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
+  import ListView from 'base/listview/listview'
+  import {getSingerList} from 'api/singer'
+  import {ERR_OK} from 'api/config'
+  import Singer from 'common/js/singer'
+
+  const HOT_SINGER_LEN = 10
+  const HOT_NAME = '热门'
+
+  export default {
+    data() {
+      return {
+        singers: []
+      }
+    },
+    created() {
+      this._getSingerList()
+    },
+    methods: {
+      handlePlaylist(playlist) {
+        const bottom = playlist.length > 0 ? '60px' : ''
+        this.$refs.singer.style.bottom = bottom
+        this.$refs.list.refresh()
+      },
+//      获取歌手列表
+      _getSingerList() {
+        getSingerList().then((res) => {
+          if(res.code === ERR_OK) {
+            this.singers = this._normalizeSinger(res.data.list)
+          }
+        })
+      },
+//      初始化歌手数据
+      _normalizeSinger(list) {
+        let map = {
+          hot: {
+            title: HOT_NAME,
+            items: []
+          }
+        }
+
+        /**
+         * 遍历歌手列表
+         * 将得到的数据的index小于10的放入到热门歌手中
+         */
+        list.forEach((item, index) => {
+          if(index < HOT_SINGER_LEN) {
+            map.hot.items.push(new Singer({
+              name: item.Fsinger_name,
+              id: item.Fsinger_mid
+            }))
+          }
+          const key = item.Findex
+//        将不重复的Findex放入到map数组中
+          if(!map[key]) {
+            map[key] = {
+              title: key,
+              items: []
+            }
+          }
+//          放入对应的歌手信息
+          map[key].items.push(new Singer({
+            name: item.Fsinger_name,
+            id: item.Fsinger_mid
+          }))
+        })
+
+//        处理map，得到有序列表
+        let ret = []
+        let hot = []
+        for (let key in map) {
+          let val = map[key]
+          if (val.title.match(/[a-zA-Z]/)) {
+            ret.push(val)
+          }else if (val.title === HOT_NAME) {
+            hot.push(val)
+          }
+        }
+        ret.sort((a, b) => {
+          return a.title.charCodeAt(0) - b.title.charCodeAt(0)
+        })
+        return hot.concat(ret)  // 将热门歌手与其他歌手拼接到一起
+        console.log(hot.concat(ret))
+      }
+    },
+    components: {
+      ListView
+    }
+  }
 </script>
 
 <style scoped lang="stylus" rel="stylesheet/stylus">
+  .singer
+    position: fixed
+    top: 88px
+    bottom: 0
+    width: 100%
 </style>
+
